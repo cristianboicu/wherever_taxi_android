b20292e5541db9d307e910d117cef1626ea5b823
Cristian <67103037+cristianboicu@users.noreply.github.com>
2022-05-11 17:16:48 +0300

    Designs Log In screen

---
+++ app/src/main/java/com/cristianboicu/wherevertaxi/ui/login/LogInFragment.kt
@@ -0,0 +1,36 @@
+package com.cristianboicu.wherevertaxi.ui.login
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.cristianboicu.wherevertaxi.R
+import com.cristianboicu.wherevertaxi.databinding.FragmentLogInBinding
+
+class LogInFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        val binding: FragmentLogInBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_log_in, container, false)
+        binding.lifecycleOwner = this
+
+        setUpUi(binding)
+        return binding.root
+    }
+
+    private fun setUpUi(binding: FragmentLogInBinding) {
+        binding.btnSignInGoogle.tvSignInWith.text = this.getString(R.string.sign_in_with_google)
+        binding.btnSignInFacebook.tvSignInWith.text = this.getString(R.string.sign_in_with_facebook)
+
+        binding.btnSignIn.setOnClickListener {
+            Log.d("LogInFragment", "Navigate Home")
+        }
+    }
+
+}
